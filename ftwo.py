4df4ac4e47ba7a12560e41f71eb36bbf22923650
Jakub Fic <jakub.f@hotmail.co.uk>
2021-01-24 13:26:47 +0000

    commit on main

--- ftwo.py
+++ ftwo.py
@@ -1,1 +1,17 @@
+"""
+Windows files renaming tool.
+The initial idea is that it works similar to 'Find and Replace' in Microsoft Excel
+"""
+
+import os
+
+class FileInfo():
+    """
+    Anything to do with renaming of the file.
+    """
+    
+    def __init__(self, filepath):
+        self.full_name = filepath
+        self.name = os.path.basename(filepath)
+        self.directory, self.extension = os.path.splitext(filepath)
 
